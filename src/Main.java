af26fd725c93e3665e224dcc423af9949a800cd3
Al01z <dampc.artur@gmail.com>
2021-12-18 04:59:25 +0100

    wgranie ćw.3

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,55 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbe: ");
+        int liczbaA = scan.nextInt();
+        switch (liczbaA%2) {
+            case 0:
+                System.out.println("Parzysta");
+                break;
+            default:
+                System.out.println("Nieprzysta");
+        }
+        switch (liczbaA%7) {
+            case 0:
+                System.out.println("Podzielna przez 7");
+                break;
+            default:
+                System.out.println("Nie podzielna przez 7");
+        }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0, 0, 0, 0};
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                    break;
+                case 'z':
+                    ++tablica[3];
+                    break;
+                case 'q':
+                    ++tablica[4];
+                    break;
+                default:
+                    ++tablica[5];
+                    break;
+
+            }
+        }
+        System.out.printf("\nLiczba wystąpień:\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy\npozostale: %d - razy",
+                tablica[0], tablica[1], tablica[2], tablica[3], tablica[4], tablica[5]);
 
 
     }
