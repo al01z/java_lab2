52ae4f08132ebb4c88eb3fb6e8c6303769b04329
Al01z <dampc.artur@gmail.com>
2021-12-18 04:44:17 +0100

    wgranie ćw.2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +9,23 @@ public class Main {
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
-
+        for (int i = 100; i >= 0; i--) {
+            if ((i%7)==0) System.out.println("Liczba " + i + " jest podzielna przez ostatnią cyfrę numeru indeksu 61527.");
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
-
+        Scanner scan = new Scanner(System.in);
+        int[] tab1 = new int[5];
+        for (int i=0; i<(tab1.length);i++) {
+            System.out.print("Podaj liczbe: ");
+            tab1[i] = scan.nextInt();
+        }
+        for (int zmienna : tab1
+             ) {
+            System.out.println(zmienna);
+        }
 
     }
 }
