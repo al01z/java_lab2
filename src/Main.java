daf76b8bbfb9d0d7fb43cde36cc6be688eec7a33
Al01z <dampc.artur@gmail.com>
2021-12-18 06:42:56 +0100

    wgranie ćw.5

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,49 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
-
-
+        List<String> lista1 = new ArrayList<>();
+        metoda1(lista1,5);
+        metoda2(lista1);
+        metoda3(lista1,2);
+        metoda1(lista1,3);
+        metoda2(lista1);
+        System.out.println(lista1.size());
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
-
+        Set<Integer> setA = new TreeSet<>();
+        metoda4(setA,10);
+        ArrayList<Integer> listA = new ArrayList<>(setA);
+        Collections.sort(listA); //Nie rozumiem chyba po co tu sortujemy
+        System.out.println("posortowany set: ");
+        for(int x: listA) {
+            System.out.print(x + " ");
+        }
+        System.out.println("liczby: " + setA);
     }
-
+    public static void metoda1(List<String> jakasLista, int liczbaWpisow) {
+        Scanner scan = new Scanner(System.in);
+        for (int i=1; i<=liczbaWpisow;i++) {
+            System.out.print("Podaj zwierze: ");
+            jakasLista.add(scan.next());
+        }
+    }
+    public static void metoda2(List<String> jakasLista) {
+        for(String zmienna : jakasLista) {
+            System.out.println(zmienna);
+        }
+    }
+    public static void metoda3(List<String> jakasLista, int liczbaWpisow) {
+        for (int i=1; i<=liczbaWpisow;i++) {
+            jakasLista.remove((jakasLista.size()-1));
+        }
+    }
+    public static void metoda4(Set<Integer> jakisSet, int liczbaWpisow) {
+        Scanner scan = new Scanner(System.in);
+        for (int i=1; i<=liczbaWpisow;i++) {
+            System.out.print("Podaj liczbe: ");
+            jakisSet.add(scan.nextInt());
+        }
+    }
 }
