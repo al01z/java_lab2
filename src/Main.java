6d68b705d024f083b43f155c897c014850ee0e52
Al01z <dampc.artur@gmail.com>
2021-12-18 05:10:46 +0100

    wgranie ćw.4

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,28 @@ public class Main {
         a) w pierwszej pętli break
         b) w drugiej pętli continue
         c) w trzeciej pętli return
-        */
+        /* przykła użycia break */
+        for (int i = 100; i >= 0; i--) {
+            if (i == 11)
+                break;
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        /* przykład użycia continue */
+        for (int i = 100; i >= 0; i--) {
+            if (i % 5 == 0)
+                continue;
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        /* przykła użycia return */
+        for (int i = 100; i >= 0; i--) {
+            if (i == 33)
+                return;
+            System.out.print(i + " ");
+        }
 
 
     }
