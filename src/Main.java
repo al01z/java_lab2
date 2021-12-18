033bd22722d63dc2aa8db5ea443190e3d7e7ee3d
Al01z <dampc.artur@gmail.com>
2021-12-18 04:31:16 +0100

    wgranie ćw.1

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,14 @@ public class Main {
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
+        int i = 1000;
+        do {
+            if ((i%27)==0) {
+                System.out.println("Liczba " + i + " jest podzielna przez dwie ostatnie cyfry numeru indeksu 61527.");
+            }
+        } while (--i > 0);
+
+
 
 
         /* zad 2
@@ -16,12 +24,33 @@ public class Main {
         b) jak wyżej tylko dla tablicy typu double[],
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
-
+        int tab1[] = new int[]{1, 3, 2, 1, 4};
+        i=0;
+        while (i < tab1.length) {
+            System.out.println(tab1[i]);
+            i++;
+        }
+        double tab2[] = new double[]{1.2, 3.6, 6.2, 11.34, 0.12};
+        i=0;
+        while (i < tab2.length) {
+            System.out.println(tab2[i]);
+            i++;
+        }
+        String tab3[] = new String[]{"raz", "dwa", "zupa", "grypa", "konik"};
+        i=tab3.length;
+        while (i > 0) {
+            System.out.println(tab3[i-1]);
+            i--;
+        }
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
-
-
+        //while (true) {
+        //    System.out.println("Raz"); // 'while' statement cannot complete without throwing an exception
+        //}
+        while (false) {
+            System.out.println("Raz"); // Condition is always false
+        }
     }
 }
